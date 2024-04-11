abff973f0b334cfde76c432669a6cbf2c2f70ed0
Jason Fischer <jasonatepaint@gmail.com>
2024-04-10 20:57:49 -0500

    no-log:  pin packages & eslint cleanup

--- src/models/clientState.ts
+++ src/models/clientState.ts
@@ -1,7 +1,7 @@
 /**
  * Represents the state passed from a client application to the SSO Broker
  */
-export interface ClientState extends Object {
+export interface ClientState {
     referrer?: string;
     [key: string]: any;
 }
